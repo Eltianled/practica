0a2644cfe023b9342af2386a1bef90e90e3e0c9e
Eltianled <tianledespinozaarevalo@gmail.com>
2024-06-06 13:19:12 -0500

    carga inicial

---
+++ src/prueba/Prueba.java
@@ -0,0 +1,22 @@
+
+package prueba;
+
+
+public class Prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int num1=6, num2=8, suma;
+        suma= num1+num2;
+        System.out.println("la suma es : "+ suma);
+        suma= suma + 5;
+        System.out.println("la otra suma es : "+ suma);
+        
+        
+             
+                
+    }
+    
+}
